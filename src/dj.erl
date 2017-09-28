de5290bced06bbbfc775f6f3fee3bef2d6b9e377
Michel Rijnders <michel.rijnders@truqu.com>
2017-09-28 14:28:08 +0200

    Add `array/1

--- src/dj.erl
+++ src/dj.erl
@@ -2,6 +2,7 @@
 
 %% API
 -export([ object/1
+        , array/1
         ]
        ).
 
@@ -15,6 +16,18 @@ object(Json) ->
     case jsx:decode(Json, [return_maps]) of
       M = #{} -> {ok, M};
       _       -> error
+    end
+  catch
+    error:_ -> error
+  end.
+
+-spec array(binary()) -> {ok, [any()]}.
+array(Json) ->
+  try
+    case jsx:decode(Json, [return_maps]) of
+      []        -> {ok, []};
+      L = [_|_] -> {ok, L};
+      _         -> error
     end
   catch
     error:_ -> error
