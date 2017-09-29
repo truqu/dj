e90691054d849856ee859f04d9ce3c83110caedd
Michel Rijnders <michel.rijnders@truqu.com>
2017-09-29 05:28:36 +0200

    Add ‘decode/2’

--- src/dj.erl
+++ src/dj.erl
@@ -1,7 +1,12 @@
 -module(dj).
 
+-ifdef(TEST).
+-include_lib("eunit/include/eunit.hrl").
+-endif.
+
 %% API
--export([ object/1
+-export([ decode/2
+        , object/1
         , array/1
         ]
        ).
@@ -9,6 +14,9 @@
 %%%-----------------------------------------------------------------------------
 %%% API
 %%%-----------------------------------------------------------------------------
+
+decode(Json, Decoders) ->
+  (compose(Decoders))(Json).
 
 -spec object(binary()) -> {ok, map()} | error.
 object(Json) ->
@@ -36,6 +44,36 @@ array(Json) ->
 %%%-----------------------------------------------------------------------------
 %%% Internal functions
 %%%-----------------------------------------------------------------------------
+
+compose(Fs) when is_list(Fs) ->
+  lists:foldl(fun compose/2, fun (X) -> X end, Fs).
+
+compose(F, G) ->
+  fun (X) -> F(G(X)) end.
+
+%%%-----------------------------------------------------------------------------
+%%% Tests
+%%%-----------------------------------------------------------------------------
+
+-ifdef(TEST).
+
+decode_object_test() ->
+  %% Test simple case
+  J = <<"{\"foo\": 42}">>,
+  M = #{foo => 42},
+  {ok, M} = dj:decode(J, [fun dj:object/1, fun dj_map:keys_as_atoms/1]),
+  %% Done
+  ok.
+
+decode_array_test() ->
+  %% Test simple case
+  J = <<"[1, 2, 3]">>,
+  L = [1, 2, 3],
+  {ok, L} = dj:decode(J, [fun dj:array/1]),
+  %% Done
+  ok.
+
+-endif.
 
 %% Local variables:
 %% mode: erlang
