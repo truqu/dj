3ea2da8580273ec8ee02cda1db5cc5f24cba2e6b
Michel Rijnders <michel.rijnders@truqu.com>
2017-10-03 11:58:01 +0200

    Add ‘uuid’ module

---
+++ src/dj_uuid.erl
@@ -0,0 +1,42 @@
+-module(dj_uuid).
+
+-ifdef(TEST).
+-include_lib("eunit/include/eunit.hrl").
+-endif.
+
+%% API
+-export([ is_v4/0
+        ]
+       ).
+
+%%%-----------------------------------------------------------------------------
+%%% API
+%%%-----------------------------------------------------------------------------
+
+is_v4() ->
+  fun
+    (X)  when is_binary(X) ->
+      case re:run(X, <<"^[0-9a-f]{8}-[0-9a-f]{4}-4[0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$">>) of
+        {match, _} -> true;
+        _          -> false
+      end;
+    (_) ->
+      false
+  end.
+
+%%%-----------------------------------------------------------------------------
+%%% Tests
+%%%-----------------------------------------------------------------------------
+
+is_v4_test() ->
+  true = (is_v4())(<<"2e2d65cb-437e-4abf-a7e8-7d97ad9d37a8">>),
+  false = (is_v4())(<<"42c1f4f4-a81c-11e7-abc4-cec278b6b50a">>),
+  ok.
+
+%% Local variables:
+%% mode: erlang
+%% erlang-indent-level: 2
+%% indent-tabs-mode: nil
+%% fill-column: 80
+%% coding: latin-1
+%% End:
