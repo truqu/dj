775a90a0215af6f0d763f43ff5d0be7723d9c2cd
ilias <mail@ilias.xyz>
2018-06-27 09:31:10 +0200

    Docs

--- src/dj.erl
+++ src/dj.erl
@@ -65,7 +65,7 @@
                | {custom, any()}
                | {invalid_json, any()}.
 
--type type() :: binary 
+-type type() :: binary
               | integer
               | pos_integer
               | neg_integer
@@ -73,7 +73,7 @@
               | null
               | boolean
               | float
-              | map 
+              | map
               | list.
 
 -type field() :: atom() | binary().
@@ -445,14 +445,34 @@ field(Field, Decoder) ->
 at(Path, Decoder) ->
   lists:foldr(fun field/2, Decoder, Path).
 
+%% @doc Decode a field as a property
+%%
+%% Similar to {@link field/2} but adds the fieldname to the decoded entry.
+%% Convenient for decoding into a proplist.
+%%
+%% @see field/2
+%% @see prop_list/1
 -spec prop(field(), decoder(T)) -> decoder({field(), T}).
 prop(Field, Decoder) ->
   map(fun (V) -> {Field, V} end, field(Field, Decoder)).
 
+%% @doc Decode a proplist by matching fields on a JSON object
 -spec prop_list([{field(), decoder(T)}]) -> decoder([{field(), T}]).
 prop_list(Spec) ->
   sequence(lists:map(fun ({F, D}) -> prop(F, D) end, Spec)).
 
+%% @doc Decode arbitrary JSON into a `map()'
+%%
+%% ```
+%%    Dec = dj:to_map(#{ x => dj:index(0, dj:integer())
+%%                     , y => dj:index(1, dj:integer())
+%%                     , z => dj:index(2, dj:integer())
+%%                     }),
+%%    Json = <<"[1, 6, 2]">>,
+%%    {ok, #{x := 1, y := 6, z := 2}} = dj:decode(Json, Dec).
+%% '''
+%%
+%% @see field/2
 -spec to_map(MapSpec) -> decoder(MapResult) when
     MapSpec :: #{Key := decoder(T)} | [{Key, decoder(T)}],
     MapResult :: #{Key := T}.
@@ -464,6 +484,16 @@ to_map(Spec) when is_list(Spec) ->
 to_map(Spec) when is_map(Spec) ->
   to_map(maps:to_list(Spec)).
 
+%% @doc Manipulate the values produced by a decoder with a function
+%%
+%% ```
+%%    Dec = dj:map(fun string:uppercase/1, dj:binary()),
+%%    Json = <<"\"hello world\"">>,
+%%    {ok, <<"HELLO WORLD">>} = dj:decode(Json, Dec).
+%% '''
+%%
+%% @see mapn/2
+%% @see chain/2
 -spec map(fun((A) -> B), decoder(A)) -> decoder(B).
 map(F, Decoder) ->
   fun (Json) ->
@@ -473,6 +503,24 @@ map(F, Decoder) ->
       end
   end.
 
+%% @doc Apply an n-ary function against a list of n decoders
+%%
+%% ```
+%%    Dec = dj:mapn( fun (X, Y, Z) -> {X, Y, Z} end
+%%                 , [ dj:field(major, dj:pos_integer())
+%%                   , dj:field(minor, dj:non_neg_integer())
+%%                   , dj:field(patch, dj:non_neg_integer())
+%%                   ]
+%%                 ),
+%%    Json = << "{ \"major\": 123"
+%%            , ", \"minor\": 66"
+%%            , ", \"patch\": 0"
+%%            , "}">>,
+%%    {ok, {123, 66, 0}} = dj:decode(Json, Dec).
+%% '''
+%%
+%% @see map/2
+%% @see chain/2
 -spec mapn(Fun, [decoder(T)]) -> decoder(V) when
     Fun :: function(),
     T   :: term(),
@@ -480,6 +528,50 @@ map(F, Decoder) ->
 mapn(Fun, Decoders) when is_function(Fun, length(Decoders))->
   map(fun (Vs) -> erlang:apply(Fun, Vs) end, sequence(Decoders)).
 
+%% @doc Chain one or more functions that create decoders onto a decoder
+%%
+%% This has many uses. One possible use is to handle data that may represent
+%% different things:
+%%
+%% ```
+%%    -type shape() :: {square, pos_integer()}
+%%                   | {oblong, pos_integer(), pos_integer()}.
+%%
+%%    -spec square() -> dj:decoder(shape()).
+%%    square() ->
+%%        dj:map(fun (S) -> {square, S} end, dj:field(side, dj:pos_integer())).
+%%
+%%    -spec oblong() -> dj:decoder(shape()).
+%%    oblong() ->
+%%        dj:map( fun(L, W) -> {oblong, L, W} end
+%%              , [ dj:field(length, dj:pos_integer())
+%%                , dj:field(width, dj:pos_integer())
+%%                ]
+%%              ).
+%%
+%%    -spec shape(square | oblong) -> dj:decoder(shape()).
+%%    shape(square) -> square();
+%%    shape(oblong) -> oblong().
+%%
+%%    -spec shape() -> dj:decoder(shape()).
+%%    shape() ->
+%%        dj:chain(dj:field(type, dj:atom([square, oblong])), fun shape/1).
+%%
+%%    {ok, {square, 12}} =
+%%        dj:decode(<<"{\"type\": \"square\", \"side\": 12}">>, shape()).
+%% '''
+%%
+%% Occasionally, you may want to chain an operation that doesn't result in a
+%% different decoder, but rather results in either failure or success. In that
+%% case, use {@link succeed/1} or {@link fail/1}.
+%%
+%% When more than one function needs to be chained (for example, a function to
+%% pattern match, and another function to validate the structure), the second
+%% argument may be a list of functions.
+%%
+%% @see map/2
+%% @see succeed/1
+%% @see fail/1
 -spec chain(decoder(A), ToDecoderB) -> decoder(B) when
     ToDecoderB :: ToDecB | [ToDecB],
     ToDecB :: fun((A) -> decoder(B)).
@@ -496,18 +588,51 @@ chain(DecoderA, ToDecoderB) ->
       end
   end.
 
+%% @doc Create a decoder that always fails with the provided term
+%%
+%% ```
+%%    Dec = dj:fail(no_more_bananas),
+%%    {error, {custom, no_more_bananas}} = dj:decode(<<"true">>, Dec).
+%% '''
+%%
+%% Mostly useful when combined with `chain'.
+%%
+%% @see chain/2
+%% @see succeed/1
 -spec fail(E :: term()) -> decoder(V :: term()).
 fail(E) ->
   fun (_) ->
       {error, {custom, E}}
   end.
 
+%% @doc Create a decoder that always succeeds with the provided term
+%%
+%% ```
+%%    Dec = dj:one_of([ dj:field(online, dj:boolean())
+%%                    , dj:succeed(false)
+%%                    ]),
+%%    Json = << "[ {\"online\": true}"
+%%            , ", {\"online\": false}"
+%%            , ", {} ]"
+%%           >>,
+%%    {ok, [true, false, false]} = dj:decode(Json, dj:list(Dec)).
+%% '''
+%%
+%% This function is also useful for hardcoding values in {@link to_map/1},
+%% handling failure and success in {@link chain/2} and - as demonstrated -
+%% defaulting values using {@link one_of/1}.
+%%
+%% @see chain/2
+%% @see to_map/1
+%% @see fail/1
+%% @see one_of/1
 -spec succeed(T) -> decoder(T).
 succeed(V) ->
   fun (_) ->
       {ok, V}
   end.
 
+%% @doc Sequence a bunch of decoders, succeeding with the collected values
 -spec sequence([decoder(T)]) -> decoder([T]).
 sequence(Decoders) ->
   fun (Json) ->
@@ -522,12 +647,14 @@ sequence(Decoders) ->
       end
   end.
 
+%% @doc Decode a JSON list using a decoder
 -spec list(decoder(T)) -> decoder([T]).
 list(Decoder) ->
   fun (Items) when is_list(Items) -> decode_all(Decoder, Items, 0, {ok, []});
       (Json)                      -> unexpected_type_error(list, Json)
   end.
 
+%% @doc Decode a single index in a JSON list using the specified decoder
 -spec index(non_neg_integer(), decoder(T)) -> decoder(T).
 index(Index, Decoder) ->
   fun (Items) when is_list(Items) ->
